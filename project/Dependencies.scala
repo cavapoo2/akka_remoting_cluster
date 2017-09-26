44aa4ddb4158f69894bf29ef390783e20b5c0654
andyr <forpetessake@protonmail.com>
2017-09-26 20:46:57 +0100

    Add files via upload

---
+++ project/Dependencies.scala
@@ -0,0 +1,7 @@
+import sbt._
+import Keys._
+object Dependencies {
+  val scalatest = "org.scalatest" %% "scalatest" % "3.0.1"
+  val akka_remote = "com.typesafe.akka" %% "akka-remote" % "2.5.3"
+
+}
