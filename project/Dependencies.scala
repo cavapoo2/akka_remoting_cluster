07680acc1b6b4950519e9be739ce970d80238578
andyr <forpetessake@protonmail.com>
2017-09-27 18:31:01 +0100

    Add files via upload

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,5 +3,6 @@ import Keys._
 object Dependencies {
   val scalatest = "org.scalatest" %% "scalatest" % "3.0.1"
   val akka_remote = "com.typesafe.akka" %% "akka-remote" % "2.5.3"
-
+  val akka_cluster = "com.typesafe.akka" %% "akka-cluster" % "2.5.4"
+  val akka_cluster_tools = "com.typesafe.akka" %% "akka-cluster-tools" % "2.5.4"
 }
