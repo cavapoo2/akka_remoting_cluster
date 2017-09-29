bd3a676eb2a9e4935f243f08e1d2553478d00cba
andyr <forpetessake@protonmail.com>
2017-09-29 18:58:28 +0100

    Add files via upload

--- src/main/scala/master.scala
+++ src/main/scala/master.scala
@@ -3,6 +3,8 @@ import akka.actor.{Actor, ActorRef, Terminated}
 import scala.util.Random
 
 case class RegisterWorker(workerActor: ActorRef)
+case object NoWorkers
+case object WorkersAvail
 
 class MasterActor extends Actor {
   var workers = List.empty[ActorRef]
@@ -16,8 +18,10 @@ class MasterActor extends Actor {
       workers = workers.filterNot(_ == actorRef)
     case work: Work if workers.isEmpty =>
       println("We cannot process your work since there is no workers.")
+      sender() ! NoWorkers
     case work: Work =>
       workers(Random.nextInt(workers.size)) ! work
+      sender() ! WorkersAvail
     case WorkDone(workId) =>
       println(s"Work with id $workId is done.")
   }
--- src/main/scala/scalingOutApp.scala
+++ src/main/scala/scalingOutApp.scala
@@ -1,16 +1,22 @@
 package com.andyr
 import akka.actor.{ActorRef, ActorSystem, Props}
+import akka.actor._
+import akka.util.Timeout
 import akka.routing.RoundRobinPool
+import akka.pattern.ask
 import com.typesafe.config.ConfigFactory
 
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Await
 import scala.concurrent.duration._
+import scala.util.{Failure, Success}
 
 object ScalingOutWorker extends App {
   if (args.size != 1)
-    {
-      println("enter a 1 or 2 to choose config file")
-      System.exit(1)
-    }
+  {
+    println("enter a 1 or 2 to choose config file")
+    System.exit(1)
+  }
   println("using:" + args(0))
   val actorSystem = ActorSystem("WorkerActorSystem",ConfigFactory.load("worker" + args(0))) //gives this port of 2554
   implicit val dispatcher = actorSystem.dispatcher
@@ -29,10 +35,31 @@ object ScalingOutWorker extends App {
 object ScalingOutMaster extends App {
   val actorSystem = ActorSystem("MasterActorSystem",ConfigFactory.load("master")) //this has port of 2552
   val masterActor = actorSystem.actorOf(Props[MasterActor], "masterActor")
-
-  (1 to 100).foreach(i => {
-    masterActor ! Work(s"$i")
-    Thread.sleep(5000) //Simulates sending work to the master actor every 5 seconds
-  })
+  def jobQueue(jobs:List[Int]): Unit = {
+    //should send a message to see if there are any workers first. if so then send them jobs
+    //alternatively the job could be queued by the master until workers are available
+    implicit val timeout = Timeout(5 seconds)
+    if (!jobs.isEmpty)
+    {
+      val j = jobs.head
+      val f = masterActor ? Work(s"$j")
+      f.onComplete {
+        case Success(value) => {
+          value match {
+            case NoWorkers =>
+              println("NoWorkers yet")
+              Thread.sleep(5000) // just simulate some delay
+              jobQueue(jobs)
+            case WorkersAvail =>
+              println("Workers Avail")
+              jobQueue(jobs.tail)
+          }
+        }
+        case Failure(e) => println(s"Fail $e")
+      }
+    }
+  }
+  //TODO create another jobQueue func that uses Await.result as well
+  jobQueue((1 to 100).toList)
 
 }
